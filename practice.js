89f337130ba9581b778352d5f8abc0e0f8499850
Emmanuel I. Obi <withtwoemms@gmail.com>
2016-04-16 15:33:46 -0500

    elm emits even on click and same elm is listening to said event. Upon hearing, emitted event, behavior is exhibited based on the state of the elm.

--- practice.js
+++ practice.js
@@ -1,11 +1,19 @@
 //-- CONSTRUCTOR ----------------------------->>>
 var Widget = function () {
 
+
 	//-- PROPERTIES ----------------------|
+	//--> elements can have data attrs that change on event
 	this.$header = $("header")
 	this.$wrapper = $("#wrapper")
 	this.$content = $("#content")
 	this.$footer = $("footer")
+	this.$aside = $("aside")
+	//------------------------------------|
+
+	//-- SET DATA ATTRIBUTES -------------|
+	this.$aside.data("isExtended", false)
+	this.$aside.data("isRetracted", true)
 	//------------------------------------|
 
 	var that = this;
@@ -23,20 +31,43 @@ var Widget = function () {
 		// call an event emitter
 		that.newEvent()
 	}
+	this.toggleDataBoolean = function (element, attrName) {
+		console.log("DATA-ATTR: ", element.data(attrName))
+		element.data(attrName) ? element.data(attrName, false) : element.data(attrName, true)
+	} 
+	this.changeElementWidth = function () { // hardcoded initially
+		if (that.$aside.data("isExtended")) {
+			that.$aside.animate({
+		    	width: 30 + "%"
+		  	}, 500)
+		} else {
+			that.$aside.animate({
+		    	width: 50 + "%"
+		  	}, 500)
+		}
+		that.toggleDataBoolean(that.$aside, "isExtended")
+	}
 	//------------------------------------|
 	
 	//-- EVENT EMITTERS ------------------|
 	this.newEvent = function () {
 		that.$content.trigger("newEvent");
 	}
+	this.toggleExt = function () {
+		that.$aside.trigger("toggleExt")
+	}
 	//------------------------------------|
+
 
 	//-- EVENT HANDLERS ------------------|
 	//--> rules bound to DOM elements that perform certain actions
 	this.$header.on("click", that.alerter)
 	this.$footer.on("click", that.logger)
 	this.$content.on("click", that.emitNewEvent)
+	this.$aside.on("click", that.toggleExt)
+
 	this.$content.on("newEvent", that.logger)
+	this.$aside.on("toggleExt", that.changeElementWidth)
 	//------------------------------------|
 } 
 //-------------------------------------------->>>
