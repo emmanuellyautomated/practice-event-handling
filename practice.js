007770b7742b4c8e4240d23e5ca9ffac940f2463
Emmanuel I. Obi <withtwoemms@gmail.com>
2016-04-16 13:30:36 -0500

    methods, emitters, & handlers; rig basic interactions

--- practice.js
+++ practice.js
@@ -1,17 +1,44 @@
-//-- CONSTRUCTOR ------------------------->>>
+//-- CONSTRUCTOR ----------------------------->>>
 var Widget = function () {
 
-//-- PROPERTIES -------------------------->>>
+	//-- PROPERTIES ----------------------|
 	this.$header = $("header")
 	this.$wrapper = $("#wrapper")
-	this.$content = $("#wrapper")
+	this.$content = $("#content")
 	this.$footer = $("footer")
-//---------------------------------------->>>
+	//------------------------------------|
 
-//-- METHODS ----------------------------->>>
-//---------------------------------------->>>
+	var that = this;
 
-//-- EVENT HANDLERS ---------------------->>>
-//---------------------------------------->>>
+	//-- METHODS -------------------------|
+	//--> each should emit at least an event
+	this.logger = function (anything) {
+		console.log(anything);
+		return anything
+	}
+	this.alerter = function () {
+		alert("ALERT!")
+	}
+	this.emitNewEvent = function () {
+		// call an event emitter
+		that.trigger()
+	}
+	//------------------------------------|
+	
+	//-- EVENT EMITTERS ------------------|
+	this.trigger = function () {
+		that.$content.trigger("newEvent");
+	}
+	//------------------------------------|
+
+	//-- EVENT HANDLERS ------------------|
+	//--> rules bound to DOM elements that perform certain actions
+	this.$header.on("click", that.alerter)
+	this.$footer.on("click", that.logger)
+	this.$content.on("click", that.emitNewEvent)
+	this.$content.on("newEvent", that.logger)
+	//------------------------------------|
 } 
-//---------------------------------------->>>
+//-------------------------------------------->>>
+
+var widget = new Widget();
