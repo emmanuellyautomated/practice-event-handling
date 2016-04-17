1c60adb6e4b28f1c838be5955037b0cc7cce6f27
Emmanuel I. Obi <withtwoemms@gmail.com>
2016-04-17 14:01:25 -0500

    some refactoring

--- practice.js
+++ practice.js
@@ -1,7 +1,6 @@
 //-- CONSTRUCTOR ----------------------------->>>
 var Widget = function () {
 
-
 	//-- PROPERTIES ----------------------|
 	//--> elements can have data attrs that change on event
 	this.$header = $("header")
@@ -9,65 +8,92 @@ var Widget = function () {
 	this.$content = $("#content")
 	this.$footer = $("footer")
 	this.$aside = $("aside")
-	//------------------------------------|
-
-	//-- SET DATA ATTRIBUTES -------------|
-	this.$aside.data("isExtended", false)
-	this.$aside.data("isRetracted", true)
+	//--> SET DATA ATTRIBUTES 
+	this.$aside.data("isShifted", false)
+	this.$aside.data("shouldRetract", false)
+	this.$aside.data("width", this.$aside[0].offsetWidth)
+	this.$content.data("isShifted", false)
+	this.$content.data("shouldRetract", true)
+	this.$content.data("width", this.$content[0].offsetWidth)
 	//------------------------------------|
 
 	var that = this;
 
 	//-- METHODS -------------------------|
-	//--> each should emit at least an event
-	this.logger = function (anything) {
+	this.log = function (anything) {
 		console.log(anything);
-		return anything
 	}
-	this.alerter = function () {
-		alert("ALERT!")
-	}
-	this.emitNewEvent = function () {
-		// call an event emitter
-		that.newEvent()
+	this.alerter = function (anything) {
+		var message = "ALERT: a <" + anything.type + "> event was triggered."
+		alert(message)
 	}
 	this.toggleDataBoolean = function (element, attrName) {
-		console.log("DATA-ATTR: ", element.data(attrName))
 		element.data(attrName) ? element.data(attrName, false) : element.data(attrName, true)
 	} 
-	this.changeElementWidth = function () { // hardcoded initially
-		if (that.$aside.data("isExtended")) {
-			that.$aside.animate({
-		    	width: 30 + "%"
-		  	}, 500)
-		} else {
-			that.$aside.animate({
-		    	width: 50 + "%"
+	this.changeElementWidth = function (e) { 
+		var percent = e.data.percent
+		var delegates = e.data.delegates
+
+		function reset(delegate) {
+			delegate.animate({
+		    	width: delegate.data("width")
 		  	}, 500)
 		}
-		that.toggleDataBoolean(that.$aside, "isExtended")
+		function shift(delegate) {
+			if (delegates[i].data("shouldRetract")) {
+				delegate.animate({
+			    	width: delegate.data("width") - percent
+			  	}, 500)
+			} else {
+				delegate.animate({
+			    	width: delegate.data("width") + percent
+			  	}, 500)
+			}
+		}
+		
+		for (var i=0; i < delegates.length; i++) {
+			if (delegates[i].data("isShifted")) {
+				reset(delegates[i])
+			} else {
+				shift(delegates[i])
+			} 
+			that.toggleDataBoolean(delegates[i], "isShifted")
+		}
 	}
 	//------------------------------------|
 	
-	//-- EVENT EMITTERS ------------------|
-	this.newEvent = function () {
-		that.$content.trigger("newEvent");
+	//-- EVENTS DELEGATORS ---------------|
+	this.newEvent = function (e) {
+		var delegates = e.data.delegates
+		for (var i=0; i < delegates.length; i++) {
+			delegates[i].trigger("newEvent");
+		}
 	}
-	this.toggleExt = function () {
-		that.$aside.trigger("toggleExt")
+	this.shiftElements = function (e) {
+		var delegates = e.data.delegates
+		for (var i=0; i < delegates.length; i++) {
+			delegates[i].trigger("shiftElements")
+		}
 	}
 	//------------------------------------|
 
-
 	//-- EVENT HANDLERS ------------------|
-	//--> rules bound to DOM elements that perform certain actions
+	//--> UI | respond to user hardware events
 	this.$header.on("click", that.alerter)
-	this.$footer.on("click", that.logger)
-	this.$content.on("click", that.emitNewEvent)
-	this.$aside.on("click", that.toggleExt)
-
-	this.$content.on("newEvent", that.logger)
-	this.$aside.on("toggleExt", that.changeElementWidth)
+	this.$footer.on("click", that.log)
+	this.$content.on("click", {
+		delegates: [that.$wrapper, that.$header]
+	}, that.newEvent)
+	this.$aside.on("click", {
+		delegates: [that.$aside]
+	}, that.shiftElements)
+	//--> UX | respond to software-generated events
+	this.$wrapper.on("newEvent", that.log)
+	this.$header.on("newEvent", that.log)
+	this.$aside.on("shiftElements", {
+		percent: 200,
+		delegates: [this.$aside, this.$content]
+	}, that.changeElementWidth)
 	//------------------------------------|
 } 
 //-------------------------------------------->>>
